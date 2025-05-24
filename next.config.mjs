e1c444030e7bab66ff3a9b5a69677abe8cd3b638
Erik Mazzelli <erikvithenzo@gmail.com>
2025-05-24 00:53:13 -0300

    Fixed environment variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    API_DOMAIN: process.env.API_DOMAIN,
+    API_PORT: process.env.API_PORT,
+  },
+};
 
 export default nextConfig;
